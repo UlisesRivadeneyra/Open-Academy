3f34d0af39f90f185dacec0bbd35196085dd013f
UlisesRivadeneyra <ulises@vauxoo.com>
2022-10-06 14:25:02 -0600

    [ADD]open_academy: Added partner module with his view T#60931
    -Added open_academy_partner_views.xml
    -Added sql constraints in open_cademy_course
    -Rewrite of the copy function in open_academy_course
    -Added new fields in open_academy_session
    -Added new api decorators in open_academy_session(constraints and compute)
    -Added new calendar view in open_academy_session_views
    -Added color variation on fields of tree in open_academy<-session_views
    -Added partner category in open_academy_partner_views

--- open_academy/models/open_academy_session.py
+++ open_academy/models/open_academy_session.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api, exceptions
+from datetime import timedelta
 
 
 class Session(models.Model):
@@ -7,7 +8,74 @@ class Session(models.Model):
 
     name = fields.Char(string="Session", required=True)
     start_date = fields.Date()
-    duration = fields.Float()
+    duration = fields.Integer()
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', ondelete="set null")
-    course_id = fields.Many2one('openacademy.course', ondelete='')
+    instructor_id = fields.Many2one(
+        'res.partner',
+        ondelete="set null",
+        domain=[
+            '|',
+            ('instructor', '=', True),
+            ('category_id.name', 'ilike', "Teacher")
+        ]
+    )
+    course_id = fields.Many2one('openacademy.course')
+    attendee_ids = fields.Many2many('res.partner')
+    taken_seats = fields.Float(compute='_compute_taken_seats')
+    start_date = fields.Date(default=fields.Date.today)
+    active = fields.Boolean(default=True)
+    end_date = fields.Date(
+        store=True,
+        compute='_compute_get_end_date',
+        inverse='_inverse_set_end_date'
+    )
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+                continue
+            start = fields.Datetime.from_string(record.start_date)
+            duration = timedelta(days=record.duration, seconds=-1)
+            record.end_date = start + duration
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for record in self:
+            if not record.seats:
+                record.taken_seats = 0.0
+            else:
+                record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
+
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for record in self:
+            if record.instructor_id in record.attendee_ids:
+                raise exceptions.ValidationError_("A session's instructor can't be an attendee")
+
+    def _set_end_date(self):
+        for record in self:
+            if not (record.start_date and record.end_date):
+                continue
+
+            start_date = fields.Datetime.from_string(record.start_date)
+            end_date = fields.Datetime.from_string(record.end_date)
+            record.duration = (end_date - start_date).days + 1
