cdb60d59016fb469d8b0bca1b6d730adbc84786f
UlisesRivadeneyra <ulises@vauxoo.com>
2022-10-13 13:41:40 -0600

    [ADD]open_academy:Added reports,wizard and i18n T#60931
    Added the folder i18n with files inside that are diferent lenguages
    for the odoo modules
    	-all.pot
    	-de.po
    	-nb_NO.po
    	-zh_HK.po
    
    Added reports folder that contains an xml to generate a report file
    and view in odoo
    	-openacademy_session_reports.xml
    
    Added wizard folder that contain an odoo wizard pop up in sessions
    model
    	-open_academy_wizard_views.xml
    	-open_academy_wizard.py

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': "OpenAcademy",
+    'license': "LGPL-3",
+    'summary': """This is the open academy course of Vauxoo""",
+    'author': "Vauxoo",
+    'website': "http://www.yourcompany.com",
+    'installable': True,
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '15.0.1.0.0',
+    # any module necessary for this one to work correctly
+    'depends': ['base','board'],
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/open_academy_course_views.xml',
+        'views/open_academy_session_views.xml',
+        'views/res_partner_views.xml',
+        'wizard/open_academy_wizard_views.xml',
+        'reports/openacademy_session_reports.xml',
+        'views/open_academy_dashboard_views.xml',        
+        'views/open_academy_menu_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/open_academy_demo.xml',
+        'demo/res_partner_category_demo.xml',
+    ],
+}
