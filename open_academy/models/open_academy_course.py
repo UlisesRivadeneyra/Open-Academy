e8eab3419bb18e07ffbebb4bee9d74ca956709b8
Ulises Rivadeneyra[Vauxoo] <ulises@vauxoo.com>
2022-10-13 09:52:53 -0600

    [ADD]open_academy: Added partner module with his view T#60931 (#4)
    * [ADD]open_academy: Added partner module with his view T#60931
    
    -Added open_academy_partner_views.xml
    -Added sql constraints in open_cademy_course
    -Rewrite of the copy function in open_academy_course
    -Added new fields in open_academy_session
    -Added new api decorators in open_academy_session(constraints and compute)
    -Added new calendar view in open_academy_session_views
    -Added color variation on fields of tree in open_academy<-session_views
    -Added partner category in open_academy_partner_views

--- open_academy/models/open_academy_course.py
+++ open_academy/models/open_academy_course.py
@@ -7,5 +7,20 @@ class Course(models.Model):
 
     title = fields.Char(string="Course", required=True)
     description = fields.Text()
+    responsible_user_id = fields.Many2one("res.users", ondelete="set null")
+    session_ids = fields.One2many(
+        "openacademy.session", inverse_name="course_id")
 
-    responsible_user = fields.Many2one("res.users", ondelete="set null")
+    _sql_constraints = [
+        (
+            'name_description_check',
+            'CHECK(title != description)',
+            "The title of the course should not be the same that description."
+        ),
+        ('name_unique', 'UNIQUE(title)', "The course title must be unique."),
+    ]
+
+    def copy(self, default=None):
+        default = dict(default or {})
+        default.update({'title': f"Copy of {self.title}"})
+        return super().copy(default)
