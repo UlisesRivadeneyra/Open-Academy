0d99679953013539fa0d1fff11831f99206f3ac0
UlisesRivadeneyra <ulises@vauxoo.com>
2022-10-14 09:43:27 -0600

    [ADD]open_academy: Added reports,wizard and i18n T#60931
    Added the folder i18n with files inside that are diferent lenguages for
    the odoo modules
    -all.pot
    -de.po
    -nb_NO.po
    -zh_HK.po
    
    Added reports folder that contains an xml to generate a report file and
    view in odoo
    -openacademy_session_reports.xml
    
    Added wizard folder that contain an odoo wizard pop up in sessions model
    -open_academy_wizard_views.xml
    -open_academy_wizard.py
    
    Added es.po file with all traductions updated

--- open_academy/__manifest__.py
+++ open_academy/__manifest__.py
@@ -11,13 +11,17 @@
     'category': 'Uncategorized',
     'version': '15.0.1.0.0',
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
     # always loaded
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/open_academy_course_views.xml',
         'views/open_academy_session_views.xml',
         'views/res_partner_views.xml',
+        'wizard/open_academy_wizard_views.xml',
+        'reports/open_academy_session_reports.xml',
+        'views/open_academy_dashboard_views.xml',
         'views/open_academy_menu_views.xml',
     ],
     # only loaded in demonstration mode
--- open_academy/models/open_academy_session.py
+++ open_academy/models/open_academy_session.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api, exceptions, _
 from datetime import timedelta
 
 
@@ -9,9 +9,9 @@ class Session(models.Model):
     active = fields.Boolean(default=True)
     attendee_ids = fields.Many2many('res.partner')
     attendees_count = fields.Integer(
-        string="Attendees count", compute='_compute_attendees_count', store=True)
+        string="Attendees", compute='_compute_attendees_count', store=True)
     color = fields.Integer()
-    course_id = fields.Many2one('openacademy.course')    
+    course_id = fields.Many2one('openacademy.course')
     duration = fields.Integer()
     end_date = fields.Date(
         store=True,
@@ -29,7 +29,7 @@ class Session(models.Model):
             ('category_id.name', 'ilike', "Teacher")
         ]
     )
-    name = fields.Char(string="Session", required=True)    
+    name = fields.Char(string="Session", required=True)
     seats = fields.Integer(string="Number of seats")
     start_date = fields.Date(default=fields.Date.today)
     taken_seats = fields.Float(compute='_compute_taken_seats')
@@ -38,20 +38,20 @@ class Session(models.Model):
         for record in self:
             if not (record.start_date and record.end_date):
                 continue
-            
+
             start_date = fields.Datetime.from_string(record.start_date)
             end_date = fields.Datetime.from_string(record.end_date)
             record.duration = (end_date - start_date).days + 1
 
     def _inverse_set_hours(self):
-        for r in self:
-            r.duration = r.hours / 24
+        for record in self:
+            record.duration = record.hours / 24
 
     @api.depends('attendee_ids')
     def _compute_attendees_count(self):
-        for r in self:
-            r.attendees_count = len(r.attendee_ids)
-    
+        for record in self:
+            record.attendees_count = len(record.attendee_ids)
+
     @api.depends('start_date', 'duration')
     def _compute_end_date(self):
         for record in self:
@@ -64,8 +64,8 @@ class Session(models.Model):
 
     @api.depends('duration')
     def _compute_get_hours(self):
-        for r in self:
-            r.hours = r.duration * 24
+        for record in self:
+            record.hours = record.duration * 24
 
     @api.depends('seats', 'attendee_ids')
     def _compute_taken_seats(self):
@@ -79,22 +79,21 @@ class Session(models.Model):
     def _check_instructor_not_in_attendees(self):
         for record in self.filtered('instructor_id'):
             if record.instructor_id in record.attendee_ids:
-                raise exceptions.ValidationError("A session's instructor can't be an attendee.")
+                raise exceptions.ValidationError(_("A session's instructor can't be an attendee."))
 
     @api.onchange('seats', 'attendee_ids')
     def _onchange_seats(self):
         if self.seats < 0:
             return {
                 'warning': {
-                    'title': "Incorrect 'seats' value.",
-                    'message': "The number of available seats may not be negative.",
+                    'title': _("Incorrect 'seats' value."),
+                    'message': _("The number of available seats may not be negative."),
                 },
             }
         if self.seats < len(self.attendee_ids):
             return {
                 'warning': {
-                    'title': "Too many attendees.",
-                    'message': "Increase seats or remove excess attendees.",
+                    'title': _("Too many attendees."),
+                    'message': _("Increase seats or remove excess attendees."),
                 },
             }
-
